ce20b1f561fd5a4642b57292de6f53e7890c165b
julian <julian@dnlab.de>
2021-04-22 21:49:22 +0200

    add query

--- src/jobs_api.py
+++ src/jobs_api.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI
 from fastapi.requests import Request
 import uvicorn
 
-from src.db import connect, setup_table
+from src.db import connect, setup_table, jobs
 from src.request_models import JobsRequestPayload
 
 conn = connect.connect_to_postgres()
@@ -16,18 +16,30 @@ setup_table.create_jobs_table(conn)
 
 app = FastAPI()
 
+
 @app.get("/health")
 def health_check():
     return {"status": "OK"}
+
 
 @app.post("/")
 def post_jobs(payload: JobsRequestPayload):
     """
     #TODO: Implement connection to DB and query
     """
-    logging.warning(payload)
-    jobs = {"job1": "Data Scientist"}
-    return jobs
+    #logging.warning(payload)
+    #jobs = {"job1": "Data Scientist"}
+    #return jobs
+    return
+
+
+@app.get("/")
+def get_jobs():
+    """
+    Return all jobs from DB
+    """
+    queried_jobs = jobs.get_jobs(conn)
+    return queried_jobs
 
 
 if __name__ == "__main__":
